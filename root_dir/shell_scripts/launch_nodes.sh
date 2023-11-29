d33c775ba5b0e3f07deb201f8153c839a017f3f8
suchetanrs <suchetan.saravanan@gmail.com>
2023-11-29 16:34:15 +0100

    Corrected shell file

--- root_dir/shell_scripts/launch_nodes.sh
+++ root_dir/shell_scripts/launch_nodes.sh
@@ -19,12 +19,12 @@ export SCOUT_Y="1.0"
 mkdir /home/$SCOUT_NAMESPACE
 ros2 launch scout_gazebo scout_simu.launch.py &
 
-sleep 50
+#sleep 50
 
-export SCOUT_NAMESPACE="scout_1"
-export SCOUT_Y="-1.0"
-mkdir /home/$SCOUT_NAMESPACE
-ros2 launch scout_gazebo scout_simu.launch.py &
+#export SCOUT_NAMESPACE="scout_1"
+#export SCOUT_Y="-1.0"
+#mkdir /home/$SCOUT_NAMESPACE
+#ros2 launch scout_gazebo scout_simu.launch.py &
 
 sleep 20
 
@@ -36,15 +36,15 @@ ros2 launch scout_navigation2 navigation.launch.py &
 ros2 launch traversability_gridmap traversability_launch.py &
 ros2 launch frontier_exploration exploration.launch.py &
 
-sleep 30
+#sleep 30
 
-export SCOUT_NAMESPACE="scout_1"
-export SCOUT_Y="-1.0"
-mkdir /home/$SCOUT_NAMESPACE
-ros2 launch scout_navigation2 scout_rtab_rgbd.launch.py &
-ros2 launch scout_navigation2 navigation.launch.py &
-ros2 launch traversability_gridmap traversability_launch.py &
-ros2 launch frontier_exploration exploration.launch.py &
+#export SCOUT_NAMESPACE="scout_1"
+#export SCOUT_Y="-1.0"
+#mkdir /home/$SCOUT_NAMESPACE
+#ros2 launch scout_navigation2 scout_rtab_rgbd.launch.py &
+#ros2 launch scout_navigation2 navigation.launch.py &
+#ros2 launch traversability_gridmap traversability_launch.py &
+#ros2 launch frontier_exploration exploration.launch.py &
 
 
 # Exploration
