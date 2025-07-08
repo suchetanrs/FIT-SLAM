591216559fa14aba5c0b48588dcf2fec700a5326
Suchetan Saravanan <suchetan@clutterbot.com>
2025-07-08 14:39:36 +0530

    Light exploration

---
+++ dev_ws/src/fit-slam2/fit_slam2/launch/light_navigation.launch.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # get share directories
+    fit_slam2_share = get_package_share_directory('fit_slam2')
+    nav2_share = get_package_share_directory('robot_navigation2')
+
+    declare_params_file = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(
+            fit_slam2_share, 'params', 'light_nav2_params.yaml'),
+        description='Path to YAML params for exploration'
+    )
+
+    # include the exploration_server.launch.py from roadmap_explorer
+    exploration_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                nav2_share,
+                'launch',
+                'navigation_fast.launch.py'
+            )
+        ),
+        launch_arguments={
+            'params_file':  LaunchConfiguration('params_file'),
+        }.items()
+    )
+
+    return LaunchDescription([
+        declare_params_file,
+        exploration_include,
+    ])
---
+++ root_dir/launch_light_exploration.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#rm -rf ~/.tmux.conf
+#rm -rf pTpUXK9A
+#wget https://pastebin.com/raw/pTpUXK9A
+#cat pTpUXK9A >> ~/.tmux.conf
+
+cd && sudo chmod +x build_packages.sh && ./build_packages.sh && cd && source ~/.bashrc
+
+SESSION_NAME="dusty_dev_tmux"
+tmux new-session -d -s $SESSION_NAME
+
+tmux split-window -h
+tmux split-window -v
+tmux select-pane -t 0
+tmux split-window -v
+tmux select-pane -t 1
+tmux split-window -v
+tmux select-pane -t 2
+tmux split-window -v
+
+tmux send-keys -t 0 "ros2 launch fit_slam2 light_exploration.launch.py" C-m
+
+tmux send-keys -t 1 "ros2 launch fit_slam2 light_navigation.launch.py" C-m
+
+tmux send-keys -t 4 "ros2 launch fit_slam2 traversability_mapping.launch.py" C-m
+
+# Attach to the tmux session
+tmux attach-session -t $SESSION_NAME
